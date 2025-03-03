182da6254ef2790560d68cd8ed1d69ef05f4100d
Regopotswe <u24736661@tuks.co.za>
2025-03-03 19:04:45 +0200

    Add project files.

---
+++ RegopotsweStudyGroup/Controllers/StudyGroupController.cs
@@ -0,0 +1,60 @@
+﻿using RegopotsweStudyGroup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace RegopotsweStudyGroup.Controllers
+{
+    public class StudyGroupController : Controller
+    {
+        // GET: StudyGroup
+        public ActionResult Index()
+        {
+            // Create a list to store study group members
+            List<StudyGroupMembers> Members = new List<StudyGroupMembers>
+            {
+                new StudyGroupMembers
+                {
+                    StudentNumber = "u24736661", 
+                    Name = "Regopotswe",
+                    Surname = "Moholane", 
+                    EmailAddress = "u24736661@tuks.co.za" 
+                },
+                new StudyGroupMembers
+                {
+                    StudentNumber = "u24735558",
+                    Name = "Palesa",
+                    Surname = "Adams",
+                    EmailAddress = "u24735558@tuks.co.za"
+                },
+                new StudyGroupMembers
+                {
+                    StudentNumber = "u34567890",
+                    Name = "Odirile",
+                    Surname = "Mmekoa",
+                    EmailAddress = "u34567890@tuks.co.za"
+                },
+                new StudyGroupMembers
+                {
+                    StudentNumber = "u45678901",
+                    Name = "Gontse",
+                    Surname = "Rashopola",
+                    EmailAddress = "45678901@tuks.co.za"
+                },
+                new StudyGroupMembers
+                {
+                    StudentNumber = "u56789012",
+                    Name = "Aaron",
+                    Surname = "Appleton",
+                    EmailAddress = "u56789012@tuks.co.za"
+                }
+            };
+
+            // showing what should be displayed to the user
+            return View(Members);
+        }
+    }
+}
---
+++ RegopotsweStudyGroup/Models/StudyGroupMembers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegopotsweStudyGroup.Models
+{
+    public class StudyGroupMembers
+    {
+
+        [Display (Name = "Student number")]
+        [Required]
+        public string StudentNumber { get; set; }
+
+        [Display (Name = "NAME")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "SURNAME")]
+        [Required]
+        public string Surname { get; set; }
+
+        [Display(Name = "EMAIL ADDRESS")]
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
+
